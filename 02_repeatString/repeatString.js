cd882f084d88f050a1ddc3b5b8005225fc6a0d50
meizu <rfsgalendez@gmail.com>
2024-03-31 00:37:13 +0800

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(ball, sack) {
+    if (sack < 0) {
+        return "ERROR"
+    }
+    let doodoo = "";
+    for (let i = 0; i < sack; i++) {
+        doodoo += ball;
+    }
+    return doodoo
 };
 
 // Do not edit below this line
