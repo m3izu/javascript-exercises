f36d192cdffaee8a45c2b6f56f529575c82e5cf8
meizu <rfsgalendez@gmail.com>
2024-04-10 17:08:38 +0800

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(toReverse) {
+    let stringy = toReverse.length;
+    let theReversed = '';
+    for (let i = stringy; i >= 0; i--) {
+        let chary = toReverse.charAt(i)
+        theReversed += chary
+    };
 
+    return theReversed;
 };
 
 // Do not edit below this line
