a0b6247602c7ae93d8d6a9d7ccb52108ead31400
meizu <rfsgalendez@gmail.com>
2024-07-08 23:43:28 +0800

    ex 2 finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, numba) {
+    if (numba < 0) return "ERROR";
+    let stwing = "";
+    for (let i = 0; i < numba; i++) {
+        stwing+=string;
+    }
+    return stwing;
+}
+    
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
