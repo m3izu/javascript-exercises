e23bd0194ece12412d9741f7c92ad636b2fe8531
meizu <rfsgalendez@gmail.com>
2024-07-19 15:45:43 +0800

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return "ERROR";
+    if (firstNum < 0 || secondNum < 0) return "ERROR";
+    if (secondNum < firstNum) {
+        const tempo = firstNum;
+        firstNum = secondNum;
+        secondNum = tempo;
+    }
 
+    let sum = 0;
+    for (i = firstNum; i <= secondNum; i++) {
+        sum += i
+    }
+    return sum;
 };
 
 // Do not edit below this line
