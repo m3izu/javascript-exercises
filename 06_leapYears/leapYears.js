ca801ba33e6fe371c851504eee43f8fcef960eb2
meizu <rfsgalendez@gmail.com>
2024-08-17 03:55:24 +0800

    finished 06, 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(leapY) {
+    const isItDivisiblebyFour = leapY % 4 === 0;
+    const isItACentury =  leapY % 100 === 0;
+    const isItDivisibleCentury = leapY % 400 === 0;
 
+    if (isItDivisiblebyFour && (!isItACentury || isItDivisibleCentury)) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+    
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  const toCelsius = (fahr - 32) * 5/9;
+  const finalC = Math.round(toCelsius * 10) / 10;
+  return finalC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celc) {
+  const toFahrenheit = (celc * 9/5) + 32
+  const finalF = Math.round(toFahrenheit * 10) / 10;
+  return finalF;
 };
 
 // Do not edit below this line
