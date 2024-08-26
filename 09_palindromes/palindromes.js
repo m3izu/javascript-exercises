3373b70d050ede6f025a2ec637271446b41053d4
meizu <rfsgalendez@gmail.com>
2024-08-27 05:25:49 +0800

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const allPossibleCharacters = 'abcdefghijklmopqrstuvwxyz1234567890';
+
+    let cleansed = word
+    .toLowerCase()
+    .split('')
+    .filter((character) => allPossibleCharacters.includes(character))
+    .join('');
+
+    const reversedWord = cleansed.split('').reverse().join('');
+
+    return cleansed == reversedWord;
 
 };
 
