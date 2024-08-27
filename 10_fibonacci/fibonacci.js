db4dfd9427293e796a36a540a0ceb74e604d157e
meizu <rfsgalendez@gmail.com>
2024-08-27 20:36:43 +0800

    done fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(infut) {
+let numb
+if (typeof infut !== 'number') {
+    numb = parseInt(infut)
+} else {
+    numb = infut
+}
 
+if (numb < 0) return "OOPS";
+if (numb == 0) return 0;
+
+let oneP = 1;
+let twoP = 0;
+
+for (let i = 2; i <= numb; i++) {
+    let current = oneP + twoP;
+    twoP = oneP;
+    oneP = current;
+}
+return oneP;
 };
 
 // Do not edit below this line
