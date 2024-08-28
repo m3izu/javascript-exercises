e806aeab5bb1e226b1e72e416159f95da7362e8e
meizu <rfsgalendez@gmail.com>
2024-08-29 01:18:59 +0800

    im habing imposter syndrome

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const getAge = function(bird, dird) {
+    if (!dird) {
+        dird = new Date().getFullYear();
+   
+    }
+    return dird - bird;};
 
-};
+    const findTheOldest = function (people) {
+        return people.reduce((tiguwang, currentFerzon) => {
+            const oldestPerson = getAge(tiguwang.yearOfBirth, tiguwang.yearOfDeath);
+            const currentAge = getAge(
+                currentFerzon.yearOfBirth,
+                currentFerzon.yearOfDeath
+            );
+    return oldestPerson < currentAge ? currentFerzon : tiguwang;
+        });
+
+    };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
